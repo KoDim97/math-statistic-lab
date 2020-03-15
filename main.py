336022b3f6625f466c67a35d9f482f26a8b5027b
KoDim97 <kodim97@yandex.ru>
2020-03-15 22:54:25 +0300

    Done 2 lab

--- main.py
+++ main.py
@@ -1,52 +1,76 @@
 import numpy as np
 import scipy.stats as stats
-import matplotlib.pyplot as plt
+import csv
 
+from prettytable import PrettyTable
 
-def createPlot(fig, data, x_data, y_data, bin, name):
-    fig.set_title(name)
-    fig.plot(x_data, y_data, label='PDF')
-    fig.hist(data, density=True, label='HIST')
-    fig.set_ylabel('Density')
-    fig.set_xlabel('x')
-    fig.legend()
-    fig.grid()
+# params
+size = [10, 100, 1000]
+l_param = float(1/2**0.5)
+u_param = float(3**0.5)
+distribution = ['Normal', 'Cauchy', 'Laplace', 'Poisson', 'Uniform']
 
+def getQuartile(distr, p):
+    n = len(distr)
+    sorted = np.sort(distr)
+    return sorted[int(np.floor(n * p) + np.ceil((n * p) - int(n * p)))]
+
+def getDistrSamples(d_name, num):
+    if d_name == 'Normal':
+        return np.random.normal(0, 1, num)
+    elif d_name == 'Cauchy':
+        return np.random.standard_cauchy(num)
+    elif d_name == 'Laplace':
+        return np.random.laplace(0, l_param, num)
+    elif d_name == 'Poisson':
+        return np.random.poisson(10, num)
+    elif d_name == 'Uniform':
+        return np.random.uniform(-u_param, u_param, num)
+    return []
 
 def main():
-    # params
-    size = [10, 50, 1000]
-    x_axis1 = np.arange(-3, 3, 0.01)
-    x_axis2 = np.arange(-5, 5, 0.01)
-    x_axis3 = np.arange(0, 20, 1)
-    l_param = float(1/2**0.5)
-    u_param = float(3**0.5)
+    for distr in distribution:
+        print(f"Distribution {distr}")
+        resTable = PrettyTable()
+        resTable.float_format = "2.2"
+        resTable.field_names = ["", "$\\bar{x}$", "$medx$", "$Z_r$", "$Z_q$", "$Z_t_r$"]
+        for d_num in size:
+            mean = []
+            med = []
+            z_r = []
+            z_q = []
+            z_tr = []
+            for it in range(1000):
+                sample_d = getDistrSamples(distr, d_num)
+                sample_d_sorted = np.sort(sample_d)
+                mean.append(np.mean(sample_d))
+                med.append(np.median(sample_d))
+                z_r.append((sample_d_sorted[0] + sample_d_sorted[-1]) / 2)
+                z_q.append((getQuartile(sample_d, 0.25) + getQuartile(sample_d, 0.75)) / 2)
+                z_tr.append(stats.trim_mean(sample_d, 0.25))
+            resTable.add_row([distr + ' n = ' + str(d_num), '','','','',''])
+            resTable.add_row(['', '', '', '', '', ''])
+            resTable.add_row(["$E(z)$",
+                              np.around(np.mean(mean), decimals=4),
+                              np.around(np.mean(med), decimals=4),
+                              np.around(np.mean(z_r), decimals=4),
+                              np.around(np.mean(z_q), decimals=4),
+                             np.around(np.mean(z_tr), decimals=4)])
+            resTable.add_row(["$D(z)$",
+                              np.around(np.std(mean) * np.std(mean), decimals=4),
+                              np.around(np.std(med) * np.std(med), decimals=4),
+                              np.around(np.std(z_r) * np.std(z_r), decimals=4),
+                              np.around(np.std(z_q) * np.std(z_q), decimals=4),
+                              np.around(np.std(z_tr) * np.std(z_tr), decimals=4)])
+            resTable.add_row(['', '', '', '', '', ''])
 
-    #results
-    fig_norm, subs_norm = plt.subplots(1, 3, figsize=(14,8))
-    fig_norm.suptitle('Normal distribution')
-    fig_cauchy, subs_cauchy = plt.subplots(1, 3, figsize=(14, 8))
-    fig_cauchy.suptitle('Сauchy distribution')
-    fig_laplace, subs_laplace = plt.subplots(1, 3, figsize=(14, 8))
-    fig_laplace.suptitle('Laplace distribution')
-    fig_poisson, subs_poisson = plt.subplots(1, 3, figsize=(14, 9))
-    fig_poisson.suptitle('Poisson distribution')
-    fig_uniform, subs_uniform = plt.subplots(1, 3, figsize=(14, 8))
-    fig_uniform.suptitle('Uniform distribution')
-    j = int(0)
-    for i in size:
-        createPlot(subs_norm[j], stats.norm.rvs(size=i), x_axis1, stats.norm.pdf(x_axis1, 0, 1), i**(1/3), "Sample = " + str(i))
-        createPlot(subs_cauchy[j], stats.cauchy.rvs(size=i), x_axis2, stats.cauchy.pdf(x_axis2), i**(1/3), "Cauchy distribution. Sample = " + str(i))
-        createPlot(subs_laplace[j], stats.laplace.rvs(0, l_param, size=i), x_axis2, stats.laplace.pdf(x_axis2, 0, l_param), i**(1/3), "Laplace distribution. Sample = " + str(i))
-        createPlot(subs_poisson[j], stats.poisson.rvs(10, size=i), x_axis3, stats.poisson.pmf(x_axis3, 10), i**(1/3), "Poisson distribution. Sample = " + str(i))
-        createPlot(subs_uniform[j], stats.uniform.rvs(-u_param, 2*u_param, size=i), [-u_param, u_param], stats.uniform.pdf([-u_param, u_param], -u_param, 2*u_param), i**(1/3), "Uniform distribution. Sample = " + str(i))
-        j += 1
-    fig_norm.savefig('Norm'+'.png')
-    fig_cauchy.savefig('Cauchy' + '.png')
-    fig_laplace.savefig('Laplace' + '.png')
-    fig_poisson.savefig('Poisson' + '.png')
-    fig_uniform.savefig('Uniform' + '.png')
+        print(resTable)
+        data = resTable.get_string()
+        result = [tuple(filter(None, map(str.strip, splitline))) for line in data.splitlines() for splitline in
+                  [line.split("|")] if len(splitline) > 1]
+        with open(distr + '.csv', 'w') as outcsv:
+            writer = csv.writer(outcsv)
+            writer.writerows(result)
 
 if __name__ == "__main__":
     main()
-
